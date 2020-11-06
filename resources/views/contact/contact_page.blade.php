cf3694921975eab15d3a39d7ca0f46dc8c1b0110
philldeveloper <philldeveloper@gmail.com>
2020-11-06 13:42:06 -0300

    added contact page

--- resources/views/contact/contact_page.blade.php
+++ resources/views/contact/contact_page.blade.php
@@ -1,99 +1,111 @@
-@extends('layouts.app')
+@extends('layouts.custom')
+
 @section('content')
-<!-- Header start -->
-@include('includes.header')
-<!-- Header end --> 
-<!-- Inner Page Title start -->
-@include('includes.inner_page_title', ['page_title'=>__('Contact Us')])
-<!-- Inner Page Title end -->
-<div class="inner-page"> 
+<!-- Masthead-->
+<header class="p-5" style="background: url('https://images.pexels.com/photos/450035/pexels-photo-450035.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'); background-repeat: no-repeat;background-attachment: scroll;background-position: center center;background-size: cover; padding-top: 15rem !important;">
+    <div class="container mx-auto">
+        <h1 class="text-light py-2">Contact Us</h1>
+    </div>
+</header>
+
+<!-- Page Title End -->
+<section class="bg-secondary-custom p-5">
     <!-- About -->
-    <div class="container">
-        <div class="contact-wrap">
-            <div class="title"> <span>{{__('We Are Here For Your Help')}}</span>
-                <h2>{{__('GET IN TOUCH FAST')}}</h2>
-                <p>
-                    {{__('Vestibulum at magna tellus. Vivamus sagittis nunc aliquet. Vivamin orci aliquam')}}
-                    <br>
-                    {{__('eros vel saphicula. Donec eget ultricies ipsmconsequat')}}
-                </p>
-            </div>
-            <div class="row"> 
-                <!-- Contact Info -->
-                <div class="contact-now">
-                    <div class="col-md-4 column">
-                        <div class="contact"> <span><i class="fa fa-home"></i></span>
-                            <div class="information"> <strong>{{__('Address')}}:</strong>
-                                <p>{{ $siteSetting->site_street_address }}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Contact Info -->
-                    <div class="col-md-4 column">
-                        <div class="contact"> <span><i class="fa fa-envelope"></i></span>
-                            <div class="information"> <strong>{{__('Email Address')}}:</strong>
-                                <p><a href="mailto:{{ $siteSetting->mail_to_address }}">{{ $siteSetting->mail_to_address }}</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Contact Info -->
-                    <div class="col-md-4 column">
-                        <div class="contact"> <span><i class="fa fa-phone"></i></span>
-                            <div class="information"> <strong>{{__('Phone')}}:</strong>
-                                <p><a href="tel:{{ $siteSetting->site_phone_primary }}">{{ $siteSetting->site_phone_primary }}</a></p>
-                                <p><a href="tel:{{ $siteSetting->site_phone_secondary }}">{{ $siteSetting->site_phone_secondary }}</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Contact Info --> 
+    <div class="container py-4">
+        <div class="card">
+            <div class="card-body">
+                <div class="text-center py-4">
+                    <h5 class="text-secondary py-2">{{__('We Are Here For Your Help')}}</h5>
+                    <h2 class="font-weight-bold">{{__('GET IN TOUCH FAST')}}</h2>
+                    <hr class="col-2">
+                    <p class="col-8 mx-auto py-3">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Nemo odio laboriosam inventore beatae illo ut, esse perferendis ducimus suscipit, 
+                        dolor magni natus cupiditate doloribus voluptatibus ad. Repellendus nulla ipsa dolorem.
+                    </p>
                 </div>
-                <div class="col-md-4 column"> 
-                    <!-- Google Map -->
-                    <div class="googlemap">
+                <div class="row justify-content-center"> 
+                    <!-- Contact Info -->
+                    <div class="col-md-4 py-4">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body">
+                                <h4>Address:</h4>
+                                <p>Rua Prof. Aprígio Gonzaga 681, Saúde, São Paulo, SP, Brasil. CEP 04303-001</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Contact Info -->
+                    <div class="col-md-4 py-4">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body">
+                                <h4>E-mail Address:</h4>
+                                <p><a class="text-dark" href="mailto:{{ $siteSetting->mail_to_address }}">{{ $siteSetting->mail_to_address }}</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Contact Info -->
+                    <div class="col-md-4 py-4">
+                        <div class="card bg-light border-0 h-100">
+                            <div class="card-body">
+                                <h4>Phone:</h4>
+                                <p class="pb-0 m-0 p-0"><a class="text-dark" href="tel:{{ $siteSetting->site_phone_primary }}">{{ $siteSetting->site_phone_primary }}</a></p>
+                                <p class="pb-0 m-0 p-0"><a class="text-dark" href="tel:{{ $siteSetting->site_phone_secondary }}">{{ $siteSetting->site_phone_secondary }}</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div><!--row-->
+
+                <h2 class="font-weight-bold text-left pl-lg-3 pl-md-3 pl-sm-0 py-3">Lorem Ipsum</h2>
+
+                <div class="row py-4">
+
+                    <div class="col-md-4"> 
+                        <!-- Google Map -->
+                        <div class="container-fluid">
                         {!! $siteSetting->site_google_map !!}
+                        </div>
                     </div>
-                </div>
-                <!-- Contact form -->
-                <div class="col-md-8 column">
-                    <div class="contact-form">
-                        <div id="message"></div>
+
+                    <div class="col-md-8">
                         <form method="post" action="{{ route('contact.us')}}" name="contactform" id="contactform">
                             {{ csrf_field() }}
                             <div class="row">
                                 <div class="col-md-6{{ $errors->has('full_name') ? ' has-error' : '' }}">                  
-                                    {!! Form::text('full_name', null, array('id'=>'full_name', 'placeholder'=>__('Full Name'), 'required'=>'required', 'autofocus'=>'autofocus')) !!}                
+                                    {!! Form::text('full_name', null, array('id'=>'full_name', 'placeholder'=>__('Full Name'), 'required'=>'required', 'class' => 'form-control mb-3', 'autofocus'=>'autofocus')) !!}                
                                     @if ($errors->has('full_name')) <span class="help-block"> <strong>{{ $errors->first('full_name') }}</strong> </span> @endif
                                 </div>
                                 <div class="col-md-6{{ $errors->has('email') ? ' has-error' : '' }}">                  
-                                    {!! Form::text('email', null, array('id'=>'email', 'placeholder'=>__('Email'), 'required'=>'required')) !!}                
+                                    {!! Form::text('email', null, array('id'=>'email', 'placeholder'=>__('Email'), 'required'=>'required', 'class' => 'form-control mb-3')) !!}                
                                     @if ($errors->has('email')) <span class="help-block"> <strong>{{ $errors->first('email') }}</strong> </span> @endif
                                 </div>
                                 <div class="col-md-6{{ $errors->has('phone') ? ' has-error' : '' }}">                  
-                                    {!! Form::text('phone', null, array('id'=>'phone', 'placeholder'=>__('Phone'))) !!}                
+                                    {!! Form::text('phone', null, array('id'=>'phone', 'class' => 'form-control mb-3', 'placeholder'=>__('Phone'))) !!}                
                                     @if ($errors->has('phone')) <span class="help-block"> <strong>{{ $errors->first('phone') }}</strong> </span> @endif
                                 </div>
                                 <div class="col-md-6{{ $errors->has('subject') ? ' has-error' : '' }}">                  
-                                    {!! Form::text('subject', null, array('id'=>'subject', 'placeholder'=>__('Subject'), 'required'=>'required')) !!}                
+                                    {!! Form::text('subject', null, array('id'=>'subject', 'class' => 'form-control mb-3', 'placeholder'=>__('Subject'), 'required'=>'required')) !!}                
                                     @if ($errors->has('subject')) <span class="help-block"> <strong>{{ $errors->first('subject') }}</strong> </span> @endif
                                 </div>
                                 <div class="col-md-12{{ $errors->has('message_txt') ? ' has-error' : '' }}">                  
-                                    {!! Form::textarea('message_txt', null, array('id'=>'message_txt', 'placeholder'=>__('Message'), 'required'=>'required')) !!}                
+                                    {!! Form::textarea('message_txt', null, array('id'=>'message_txt', 'class' => 'form-control mb-3', 'placeholder'=>__('Message'), 'required'=>'required')) !!}                
                                     @if ($errors->has('message_txt')) <span class="help-block"> <strong>{{ $errors->first('message_txt') }}</strong> </span> @endif
                                 </div>
                                 <div class="col-md-12{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
                                     {!! app('captcha')->display() !!}
                                     @if ($errors->has('g-recaptcha-response')) <span class="help-block"> <strong>{{ $errors->first('g-recaptcha-response') }}</strong> </span> @endif
                                 </div>
-                                <div class="col-md-12">
-                                    <button title="" class="button" type="submit" id="submit">{{__('Submit Now')}}</button>
+                                <div class="col-md-12 py-3">
+                                    <button title="" class="btn btn-primary btn-lg" type="submit" id="submit">{{__('Submit Now')}}</button>
                                 </div>
                             </div>
                         </form>
                     </div>
-                </div>
-            </div>
+
+                </div><!--row-->
+
+            </div><!--card-body-->
         </div>
     </div>
-</div>
-@include('includes.footer')
+</section>
 @endsection
---
+++ resources/views/front-end/companies/companies.blade.php
@@ -0,0 +1,121 @@
+@extends('layouts.custom')
+
+@section('content')
+<!-- Masthead-->
+<header class="p-5" style="background: url('https://images.pexels.com/photos/450035/pexels-photo-450035.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'); background-repeat: no-repeat;background-attachment: scroll;background-position: center center;background-size: cover; padding-top: 15rem !important;">
+    <div class="container mx-auto">
+        <h1 class="text-light py-2">Companies List</h1>
+    </div>
+</header>
+
+<!-- Feature Jobs-->
+<section class="page-section bg-secondary-custom" id="services">
+    <div class="container mb-5">
+        <div class="row">
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="card portfolio-item border rounded shadow-sm">
+                    <a class="card-header portfolio-link p-0 border-0" href="{{asset('/teste/company')}}">
+                        <img class="img-fluid" src="{{asset('admin_assets/no-image.png')}}" alt="" />
+                    </a>
+                    <div class="card-body portfolio-caption">
+                        <div class="portfolio-caption-heading py-1 text-left">Electrical Engineer</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">Your Location Address USA</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">
+                            <div class="badge p-2 mt-3 badge-info">Current jobs : 1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="card portfolio-item border rounded shadow-sm">
+                    <a class="card-header portfolio-link p-0 border-0" href="{{asset('/teste/company')}}">
+                        <img class="img-fluid" src="{{asset('admin_assets/no-image.png')}}" alt="" />
+                    </a>
+                    <div class="card-body portfolio-caption">
+                        <div class="portfolio-caption-heading py-1 text-left">Electrical Engineer</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">Your Location Address USA</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">
+                            <div class="badge p-2 mt-3 badge-info">Current jobs : 1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="card portfolio-item border rounded shadow-sm">
+                    <a class="card-header portfolio-link p-0 border-0" href="{{asset('/teste/company')}}">
+                        <img class="img-fluid" src="{{asset('admin_assets/no-image.png')}}" alt="" />
+                    </a>
+                    <div class="card-body portfolio-caption">
+                        <div class="portfolio-caption-heading py-1 text-left">Electrical Engineer</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">Your Location Address USA</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">
+                            <div class="badge p-2 mt-3 badge-info">Current jobs : 1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="card portfolio-item border rounded shadow-sm">
+                    <a class="card-header portfolio-link p-0 border-0" href="{{asset('/teste/company')}}">
+                        <img class="img-fluid" src="{{asset('admin_assets/no-image.png')}}" alt="" />
+                    </a>
+                    <div class="card-body portfolio-caption">
+                        <div class="portfolio-caption-heading py-1 text-left">Electrical Engineer</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">Your Location Address USA</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">
+                            <div class="badge p-2 mt-3 badge-info">Current jobs : 1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="card portfolio-item border rounded shadow-sm">
+                    <a class="card-header portfolio-link p-0 border-0" href="{{asset('/teste/company')}}">
+                        <img class="img-fluid" src="{{asset('admin_assets/no-image.png')}}" alt="" />
+                    </a>
+                    <div class="card-body portfolio-caption">
+                        <div class="portfolio-caption-heading py-1 text-left">Electrical Engineer</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">Your Location Address USA</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">
+                            <div class="badge p-2 mt-3 badge-info">Current jobs : 1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-sm-6 mb-4">
+                <div class="card portfolio-item border rounded shadow-sm">
+                    <a class="card-header portfolio-link p-0 border-0" href="{{asset('/teste/company')}}">
+                        <img class="img-fluid" src="{{asset('admin_assets/no-image.png')}}" alt="" />
+                    </a>
+                    <div class="card-body portfolio-caption">
+                        <div class="portfolio-caption-heading py-1 text-left">Electrical Engineer</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">Your Location Address USA</div>
+                        <div class="portfolio-caption-subheading text-left text-muted">
+                            <div class="badge p-2 mt-3 badge-info">Current jobs : 1</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--pagination-->
+        <div class="row justify-content-between py-4">
+            <div class="col-md-5">
+                <div class="showreslt">
+                    Showing Pages : 11 - 20 Total 43
+                </div>
+            </div>
+            <div class="col-md-7">
+                <ul class="pagination float-right">
+                    <li class="page-item"><a class="page-link" href="http://vps23865.publiccloud.com.br/jobs?page=1" rel="prev">«</a></li>
+                    <li class="page-item"><a class="page-link" href="http://vps23865.publiccloud.com.br/jobs?page=1">1</a></li>
+                    <li class="page-item active"><span class="page-link">2</span></li>
+                    <li class="page-item"><a class="page-link" href="http://vps23865.publiccloud.com.br/jobs?page=3">3</a></li>
+                    <li class="page-item"><a class="page-link" href="http://vps23865.publiccloud.com.br/jobs?page=4">4</a></li>
+                    <li class="page-item"><a class="page-link" href="http://vps23865.publiccloud.com.br/jobs?page=5">5</a></li>     
+                    <li class="page-item"><a class="page-link" href="http://vps23865.publiccloud.com.br/jobs?page=3" rel="next">»</a></li>
+                </ul>
+            </div>
+        </div><!--end of pagination-->
+    </div><!--row-->
+</section>
+@endsection
