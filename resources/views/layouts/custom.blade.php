f07412d8e34ecbf2eed1b7088f0344bd5fc14761
philldeveloper <philldeveloper@gmail.com>
2020-11-06 15:46:28 -0300

    adding new navbar

--- resources/views/layouts/custom.blade.php
+++ resources/views/layouts/custom.blade.php
@@ -24,26 +24,33 @@ if (!isset($seo)) {
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{asset('css/styles.css')}}" rel="stylesheet" />
         <link href="{{asset('css/custom.css')}}" rel="stylesheet" />
+
+        <style>
+            #mainNav{
+                transition: 0 !important;
+            }
+        </style>
     </head>
 
     <body id="page-top">
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+
+        <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light shadow-sm">
             <div class="container">
-                <!-- <a class="navbar-brand text-white js-scroll-trigger" href="#page-top">Freela <b class="text-info">Security</b></a> -->
-                <a class="navbar-brand text-white js-scroll-trigger" href="{{asset('/teste')}}"><img src="{{asset('img/logos/logo_two.png')}}" alt=""></a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars ml-1"></i>
+                <a class="navbar-brand mr-0 mr-md-2" href="{{asset('/teste')}}">
+                    <img src="{{asset('img/logos/main-logo.png')}}" alt="" width="auto" height="36">
+                </a>
+                <!-- <a class="navbar-brand mr-0 mr-md-2" href="/" aria-label="Bootstrap"><svg class="d-block" width="36" height="36" viewBox="0 0 612 612" xmlns="http://www.w3.org/2000/svg" focusable="false"><title>Bootstrap</title><path fill="currentColor" d="M510 8a94.3 94.3 0 0 1 94 94v408a94.3 94.3 0 0 1-94 94H102a94.3 94.3 0 0 1-94-94V102a94.3 94.3 0 0 1 94-94h408m0-8H102C45.9 0 0 45.9 0 102v408c0 56.1 45.9 102 102 102h408c56.1 0 102-45.9 102-102V102C612 45.9 566.1 0 510 0z"></path><path fill="currentColor" d="M196.77 471.5V154.43h124.15c54.27 0 91 31.64 91 79.1 0 33-24.17 63.72-54.71 69.21v1.76c43.07 5.49 70.75 35.82 70.75 78 0 55.81-40 89-107.45 89zm39.55-180.4h63.28c46.8 0 72.29-18.68 72.29-53 0-31.42-21.53-48.78-60-48.78h-75.57zm78.22 145.46c47.68 0 72.73-19.34 72.73-56s-25.93-55.37-76.46-55.37h-74.49v111.4z"></path></svg></a> -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav text-uppercase ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="{{asset('/teste/jobs')}}">Jobs</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{asset('/teste/companies')}}">Companies</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{asset('/contact-us')}}">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link btn btn-primary" href="{{asset('/login')}}">Sign In</a></li>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="nav navbar-nav navbar-right ml-auto">
+                        <li class="nav-item"><a class="nav-link h5 mt-1" href="{{asset('/teste/jobs')}}">Jobs</a></li>
+                        <li class="nav-item"><a class="nav-link h5 mt-1" href="{{asset('/teste/companies')}}">Companies</a></li>
+                        <li class="nav-item"><a class="nav-link h5 mt-1" href="{{asset('/contact-us')}}">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link h5 mt-1 ml-lg-3 pl-lg-3 btn btn-primary text-light font-weight-bold" href="{{asset('/login')}}"><i class="fa fa-user mr-2"></i>Sign In</a></li>
                     </ul>
-                </div>
+                </div>          
             </div>
         </nav>
 
