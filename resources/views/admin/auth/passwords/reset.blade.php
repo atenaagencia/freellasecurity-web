525f0aa91a607947fdeeca2df6890527f34c5e7c
Herbet Junior <herbetjr@gmail.com>
2021-11-04 16:25:36 -0300

    ds

--- resources/views/admin/auth/passwords/reset.blade.php
+++ resources/views/admin/auth/passwords/reset.blade.php
@@ -6,7 +6,7 @@
     <form class="form-horizontal" role="form" method="POST" action="{{ route('admin.password.request') }}">
         {{ csrf_field() }}
         <input type="hidden" name="token" value="{{ $token }}">
-        <h3 class="font-green">Reset Password</h3>
+        <h3 class="font-green">Resetar Senha</h3>
         @if ($errors->has('email'))
         <div class="alert alert-danger">
             <button class="close" data-close="alert"></button>
@@ -27,21 +27,21 @@
         @endif        
         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
             <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-            <label class="control-label visible-ie8 visible-ie9">E-Mail Address</label>
-            <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off" placeholder="Email Address" name="email" value="{{ old('email', $email) }}" />                   
+            <label class="control-label visible-ie8 visible-ie9">E-Mail</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="text" autocomplete="off" placeholder="Email Cadastrado" name="email" value="{{ old('email', $email) }}" />                   
         </div>
         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
             <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-            <label class="control-label visible-ie8 visible-ie9">Password</label>
-            <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="password" name="password" />  
+            <label class="control-label visible-ie8 visible-ie9">Senha</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="Senha" name="password" />  
         </div>
         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
             <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-            <label class="control-label visible-ie8 visible-ie9">Confirm Password</label>
-            <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="confirm password" name="password_confirmation" />  
+            <label class="control-label visible-ie8 visible-ie9">Confirme Senha</label>
+            <input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="Confirme Senha" name="password_confirmation" />  
         </div>
         <div class="form-actions">
-            <button type="submit" class="btn btn-success uppercase pull-right">Reset Password</button>
+            <button type="submit" class="btn btn-success uppercase pull-right">Resetar Senha</button>
         </div>
     </form>
     <!-- END FORGOT PASSWORD FORM -->
