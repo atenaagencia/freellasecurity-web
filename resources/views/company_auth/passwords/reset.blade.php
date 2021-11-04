1118456d969fd4b3608bd67d893caba470358010
Herbet Junior <herbetjr@gmail.com>
2021-11-04 16:54:51 -0300

    dsds

--- resources/views/company_auth/passwords/reset.blade.php
+++ resources/views/company_auth/passwords/reset.blade.php
@@ -40,7 +40,7 @@
                         {{ csrf_field() }}
                         <input type="hidden" name="token" value="{{ $token }}">
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">{{__('Email Address')}}</label>
+                            <label for="email" class="col-md-4 control-label">{{__('Email ')}}</label>
                             <div class="col-md-12">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
                                 @if ($errors->has('email'))
@@ -51,7 +51,7 @@
                             </div>
                         </div>
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">{{__('Password')}}</label>
+                            <label for="password" class="col-md-4 control-label">{{__('Senha')}}</label>
                             <div class="col-md-12">
                                 <input id="password" type="password" class="form-control" name="password" required>
                                 @if ($errors->has('password'))
@@ -62,7 +62,7 @@
                             </div>
                         </div>
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">{{__('Confirm Password')}}</label>
+                            <label for="password-confirm" class="col-md-4 control-label">{{__('Confirme Senha')}}</label>
                             <div class="col-md-12">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                                 @if ($errors->has('password_confirmation'))
