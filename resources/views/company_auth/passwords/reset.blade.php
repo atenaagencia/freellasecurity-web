e1500aa0d0d1be33ecd0b0b02bc7154c2440df94
Herbet Junior <herbetjr@gmail.com>
2021-11-04 16:55:29 -0300

    dsd

--- resources/views/company_auth/passwords/reset.blade.php
+++ resources/views/company_auth/passwords/reset.blade.php
@@ -75,7 +75,7 @@
                         <div class="form-group">
                             <div class="col-md-12">
                                 <button type="submit" class="btn btn-dark">
-                                    {{__('Reset Password')}}
+                                    {{__('Resetar Senha')}}
                                 </button>
                             </div>
                         </div>
